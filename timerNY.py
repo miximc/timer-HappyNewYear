f43a2a5749567c3733762780406d5e31fb75ffbe
mixi <miximc@mail.ru>
2022-12-13 22:14:13 +0300

    добавил коментарии и изменил размер окна

--- timerNY.py
+++ timerNY.py
@@ -13,13 +13,14 @@ def new_year(window):
     timer = new_y - my_time
     timer -= timedelta(microseconds=timer.microseconds)
     a = f'До нового года осталось: {timer}'
-    lbl = Label(window, text=a)
+    lbl = Label(window, text=a)         # вывод времени дни, часы, минуты и секунды
     lbl.grid(row= 0, column=1)
     window.after(1000, new_year, window)
     seconds = timer.seconds + timer.days * 86400
     seconds_2 = f'До нового года осталось: {seconds} сек'
-    lbl_2 = Label(window, text=seconds_2)
+    lbl_2 = Label(window, text=seconds_2) # вывод времени секундами
     lbl_2.grid(row=2, column=1)
+
 new_year(window)
-window.geometry('250x250')
+window.geometry('240x260')
 window.mainloop()  
