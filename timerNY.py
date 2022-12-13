c79a41991a529286058aabcd0a9be9dbf4dc034b
mixi <miximc@mail.ru>
2022-12-13 22:09:55 +0300

    Таймер - дни и секунды

---
+++ timerNY.py
@@ -0,0 +1,25 @@
+# таймер до нового года
+import datetime
+from datetime import timedelta
+from tkinter import *
+
+window = Tk()
+window.title('Таймер')
+
+def new_year(window):
+    
+    new_y = datetime.datetime(2023, 1, 1,0,0)
+    my_time = datetime.datetime.today()    
+    timer = new_y - my_time
+    timer -= timedelta(microseconds=timer.microseconds)
+    a = f'До нового года осталось: {timer}'
+    lbl = Label(window, text=a)
+    lbl.grid(row= 0, column=1)
+    window.after(1000, new_year, window)
+    seconds = timer.seconds + timer.days * 86400
+    seconds_2 = f'До нового года осталось: {seconds} сек'
+    lbl_2 = Label(window, text=seconds_2)
+    lbl_2.grid(row=2, column=1)
+new_year(window)
+window.geometry('250x250')
+window.mainloop()  
